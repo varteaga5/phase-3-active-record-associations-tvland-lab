64fbb4c98e81fd0dd4e4fa4ff9e6bcbd6f04a70e
vincent <vincent.arteaga@gmail.com>
2021-08-19 19:46:20 -0700

    lab complete

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,10 @@
 class Character < ActiveRecord::Base
-  
+    # returns the actor that the character belongs to
+    belongs_to :actor
+    # returns the show that the character belongs to 
+    belongs_to :show
+
+    def say_that_thing_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+    end
 end
--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,5 +1,8 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    # add_column(table_name, column_name, type, **options)Lin
+    add_column(:characters, :catchphrase, :string)
+
   end
 end
